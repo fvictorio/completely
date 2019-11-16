b39a5dce4f648fce0b20e048d71bcf44653a717d
Franco Victorio <victorio.franco@gmail.com>
2019-11-15 22:02:54 -0300

    Add support for single commands, for colons, and for using other commands for completion

--- packages/bash-generator/src/index.ts
+++ packages/bash-generator/src/index.ts
@@ -2,50 +2,83 @@ import * as fs from 'fs'
 import * as path from 'path'
 import Handlebars from 'handlebars';
 
+Handlebars.registerHelper('commandsExist', (commands: string[]) => {
+  if (!commands || !commands.length) {
+    return 'true'
+  }
+  const commandsConditions = commands
+    .map(command => `command -v ${command} > /dev/null`)
+
+  const condition = commandsConditions.join(' && ')
+
+  return condition
+})
+
 const template = fs.readFileSync(path.join(__dirname, 'template.sh')).toString()
 
 const compiled = Handlebars.compile(template);
 
+
 export const generate = (completionSpec: any): string => {
   const commandName = completionSpec.command;
-  const subcommandsList = completionSpec.subcommands
+  const subcommandsList = (completionSpec.subcommands || [])
     .map((subcommand: any) => subcommand.name)
     .join(' ');
-  const subcommands = completionSpec.subcommands.map((subcommand: any) => {
+
+  const rootCommand = {
+    ...completionSpec,
+    name: "",
+    subcommands: null
+  }
+
+  const allCommands = completionSpec.subcommands ? (completionSpec.subcommands || []) : [rootCommand]
+
+  const subcommands = allCommands.map((subcommand: any) => {
     const name = subcommand.name;
 
     const booleanFlags = (subcommand.flags || [])
       .filter((flag: any) => flag.type === 'boolean')
-      .map((flag: any) => ({ longName: flag.name }));
+      .map((flag: any) => ({ longName: flag.name, shortName: flag.char }));
     const stringFlags = (subcommand.flags || [])
       .filter((flag: any) => flag.type === 'string')
       .map((flag: any) => {
-        const completionFiles = flag.completion.type === 'files';
-        const completionDirectories = flag.completion.type === 'directories';
-        const completionOneOf = flag.completion.type === 'oneOf';
-
-        const result: any = { longName: flag.name };
+        const result: any = { longName: flag.name, shortName: flag.char };
         result.completion = {};
-        if (completionFiles) {
-          result.completion.files = true;
-        } else if (completionDirectories) {
-          result.completion.directories = true;
-        } else if (completionOneOf) {
-          result.completion.oneOf = flag.completion.values.join(' ');
+        switch (flag.completion.type) {
+          case 'files':
+            result.completion.files = true;
+            break;
+          case 'directories':
+            result.completion.directories = true;
+            break;
+          case 'oneOf':
+            result.completion.oneOf = flag.completion.values.join(' ');
+            break;
+          case 'command':
+            result.completion.command = flag.completion.command;
+            result.completion.requiredCommands = flag.completion.requiredCommands;
+            break;
         }
 
         return result;
       });
 
     const args = (subcommand.args || []).map((arg: any) => {
-      const completionFiles = arg.completion.type === 'files';
-      const completionDirectories = arg.completion.type === 'directories';
-
       const result: any = { completion: {} };
-      if (completionFiles) {
-        result.completion.files = true;
-      } else if (completionDirectories) {
-        result.completion.directories = true;
+      switch (arg.completion.type) {
+        case 'files':
+          result.completion.files = true;
+          break;
+        case 'directories':
+          result.completion.directories = true;
+          break;
+        case 'oneOf':
+          result.completion.oneOf = arg.completion.values.join(' ');
+          break;
+        case 'command':
+          result.completion.command = arg.completion.command;
+          result.completion.requiredCommands = arg.completion.requiredCommands;
+          break;
       }
 
       return result;
--- packages/bash-generator/src/template.sh
+++ packages/bash-generator/src/template.sh
@@ -10,30 +10,38 @@ declare -f _contains_element > /dev/null || _contains_element() {
 
 _{{commandName}}_completions()
 {
-  local cur="${COMP_WORDS[$COMP_CWORD]}" # current word
+  # get current word, words array, current word index, and previous word, ignoring ":" as a wordbreak
+  local cur cword words
+  _get_comp_words_by_ref -n ":" cur words cword prev
 
   # complete subcommands list
-  if [ "$COMP_CWORD" -eq "1" ]; then
+  if [ "$cword" -eq "1" ] && [ "{{ subcommandsList }}" != "" ]; then
     COMPREPLY=($(compgen -W "{{ subcommandsList }}" -- "$cur"))
+    __ltrim_colon_completions "$cur"
     return
   fi
 
-  local subcommand="${COMP_WORDS[1]}"
-  local prev="${COMP_WORDS[$COMP_CWORD - 1]}"
+  local subcommand="${words[1]}"
 
   local args used_flags used_args index
-  args=("${COMP_WORDS[@]:2}") # args without command and subcommand
 
   # register completions for each subcommand
   {{#each subcommands}}
+  {{#if name}}
   if [ "${subcommand}" == "{{name}}" ]; then
+    local args_shift=2
+  {{else}}
+  if true; then
+    local args_shift=1
+  {{/if}}
     # get the list of already used flags and args, ignoring the current word
+    args=("${words[@]:args_shift}") # args without command and subcommand
     used_flags=()
     used_args=()
     index=0
 
     while [ "${#args[@]}" -gt 0 ]; do
-      if [ "${index}" -eq "$((COMP_CWORD-2))" ]; then
+      if [ "${index}" -eq "$((cword-args_shift))" ]; then
         # ignore current word
         args=("${args[@]:1}")
         index=$((index+1))
@@ -41,8 +49,12 @@ _{{commandName}}_completions()
       fi
 
       {{#each booleanFlags}}
+      {{#if shortName}}
+      if [ "${args[0]}" == "--{{longName}}" ] || [ "${args[0]}" == "-{{shortName}}" ]; then
+      {{else}}
       if [ "${args[0]}" == "--{{longName}}" ]; then
-        used_flags+=("--{{longName}}")
+      {{/if}}
+        used_flags+=("${args[0]}")
         args=("${args[@]:1}")
         index=$((index+1))
         continue
@@ -50,21 +62,31 @@ _{{commandName}}_completions()
       {{/each}}
 
       {{#each stringFlags}}
+      {{#if shortName}}
+      if [ "${args[0]}" == "--{{longName}}" ] || [ "${args[0]}" == "-{{shortName}}" ]; then
+      {{else}}
       if [ "${args[0]}" == "--{{longName}}" ]; then
-        used_flags+=("--{{longName}}")
+      {{/if}}
+        used_flags+=("${args[0]}")
         args=("${args[@]:2}")
         index=$((index+2))
         continue
       fi
       {{/each}}
 
-      used_args+=("${args[0]}")
+      if [[ "${args[0]}" != "-"* ]]; then
+        used_args+=("${args[0]}")
+      fi
       args=("${args[@]:1}")
       index=$((index+1))
     done
 
     {{#each stringFlags}}
+    {{#if shortName}}
+    if [ $prev == "--{{longName}}" ] || [ $prev == "-{{shortName}}" ]; then
+    {{else}}
     if [[ $prev == "--{{longName}}" ]]; then
+    {{/if}}
       COMPREPLY=()
       {{#if completion.files}}
       COMPREPLY=($(compgen -f -- "$cur"))
@@ -83,6 +105,24 @@ _{{commandName}}_completions()
       # flags
       completion=()
 
+      if [[ $cur != --* ]]; then
+        true
+        {{#each booleanFlags}}
+          {{#if shortName}}
+          if ! _contains_element "-{{shortName}}" "${used_flags[@]}"; then
+            completion+=("-{{shortName}}")
+          fi
+          {{/if}}
+        {{/each}}
+        {{#each stringFlags}}
+          {{#if shortName}}
+          if ! _contains_element "-{{shortName}}" "${used_flags[@]}"; then
+            completion+=("-{{shortName}}")
+          fi
+          {{/if}}
+        {{/each}}
+      fi
+
       {{#each booleanFlags}}
         if ! _contains_element "--{{longName}}" "${used_flags[@]}"; then
           completion+=("--{{longName}}")
@@ -93,6 +133,7 @@ _{{commandName}}_completions()
           completion+=("--{{longName}}")
         fi
       {{/each}}
+
       COMPREPLY=($(compgen -W "${completion[*]}" -- "$cur"))
       return
     fi
@@ -105,6 +146,14 @@ _{{commandName}}_completions()
       {{/if}}
       {{#if completion.directories}}
       COMPREPLY=($(compgen -d -- "$cur"))
+      {{/if}}
+      {{#if completion.oneOf}}
+      COMPREPLY=($(compgen -W "{{ completion.oneOf }}" -- "$cur"))
+      {{/if}}
+      {{#if completion.command}}
+      if {{#commandsExist completion.requiredCommands}}{{/commandsExist}}; then
+        COMPREPLY=($(compgen -W "$({{{completion.command}}})" -- "$cur"))
+      fi
       {{/if}}
       return
     fi
