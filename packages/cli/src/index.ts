d58efe21853d872b5cc6d30711c500a32ef8e3a5
Franco Victorio <victorio.franco@gmail.com>
2019-11-18 17:31:07 -0300

    Add shell flag to cli

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,26 +1,26 @@
-import betterAjvErrors from 'better-ajv-errors';
+import betterAjvErrors from 'better-ajv-errors'
 import * as fs from 'fs'
 import { generate } from '@completely/bash-generator'
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from '@oclif/command'
 import { schema, validate } from '@completely/spec'
 
 class Completely extends Command {
   static description = 'Generate a shell completion script from a JSON description of your command'
 
   static flags = {
-    // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    version: flags.version({ char: 'v' }),
+    help: flags.help({ char: 'h' }),
+    shell: flags.string({
+      description: 'Choose a shell for generating the completion file',
+      options: ['bash'],
+      default: 'bash'
+    })
   }
 
-  static args = [{name: 'file'}]
+  static args = [{ name: 'file' }]
 
   async run() {
-    const {args, flags} = this.parse(Completely)
+    const { args } = this.parse(Completely)
 
     let completionSpec
     try {
@@ -31,7 +31,7 @@ class Completely extends Command {
 
     const valid = validate(completionSpec)
     if (!valid) {
-      const output = betterAjvErrors(schema, completionSpec, validate.errors, { indent: 2 });
+      const output = betterAjvErrors(schema, completionSpec, validate.errors, { indent: 2 })
       this.error(output as any)
     }
 
